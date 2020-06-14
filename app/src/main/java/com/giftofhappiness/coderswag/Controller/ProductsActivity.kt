aa6463fa8a23d40ff259d904e3d4430d46cd6662
jlawrencesha <44271286+jlawrencesha@users.noreply.github.com>
2020-06-14 16:36:45 -0700

    new changes but not working properly.

--- app/src/main/java/com/giftofhappiness/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/giftofhappiness/coderswag/Controller/ProductsActivity.kt
@@ -2,16 +2,29 @@ package com.giftofhappiness.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.giftofhappiness.coderswag.Adapters.ProductsAdapter
 import com.giftofhappiness.coderswag.R
+import com.giftofhappiness.coderswag.Services.DataService
 import com.giftofhappiness.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
        // intent.getStringExtra(EXTRA_CATEGORY)
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this,DataService.getProducts(categoryType))
+
+        val layoutManager = GridLayoutManager(this, 2)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
+
+
     }
 }
--- app/src/main/java/com/giftofhappiness/coderswag/Services/DataService.kt
+++ app/src/main/java/com/giftofhappiness/coderswag/Services/DataService.kt
@@ -25,11 +25,31 @@ object DataService {
         Product("Devslopes Graphic Bean", "$18", "hat1"),
         Product("Devslopes Hat Black", "$20", "hat2"),
         Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Bean", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Bean", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Bean", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
         Product("Devslopes Hat Snapback", "$22", "hat4")
     )
 
     val hoodies = listOf(
 
+        Product("Devlopes Hoodie Gray", "$28", "hoodie1"),
+        Product("Devlopes Hoodie Red", "$32", "hoodie2"),
+        Product("Devlopes Gray Hoodie", "$28", "hoodie3"),
+        Product("Devlopes Hoodie Black", "$32", "hoodie4"),
+        Product("Devlopes Hoodie Gray", "$28", "hoodie1"),
+        Product("Devlopes Hoodie Red", "$32", "hoodie2"),
+        Product("Devlopes Gray Hoodie", "$28", "hoodie3"),
+        Product("Devlopes Hoodie Black", "$32", "hoodie4"),
         Product("Devlopes Hoodie Gray", "$28", "hoodie1"),
         Product("Devlopes Hoodie Red", "$32", "hoodie2"),
         Product("Devlopes Gray Hoodie", "$28", "hoodie3"),
@@ -41,8 +61,17 @@ object DataService {
         Product("Devlopes Badge Light Gray", "$20", "shirt2"),
         Product("Devlopes Logo Shirt Red", "$22", "shirt3"),
         Product("Devlopes Hustle", "$22", "shirt4") ,
+        Product("Kickflip Studios", "$18", "shirt5"),
+        Product("Devlopes Shirt Black", "$18", "shirt1"),
+        Product("Devlopes Badge Light Gray", "$20", "shirt2"),
+        Product("Devlopes Logo Shirt Red", "$22", "shirt3"),
+        Product("Devlopes Hustle", "$22", "shirt4") ,
+        Product("Kickflip Studios", "$18", "shirt5"),
+        Product("Devlopes Shirt Black", "$18", "shirt1"),
+        Product("Devlopes Badge Light Gray", "$20", "shirt2"),
+        Product("Devlopes Logo Shirt Red", "$22", "shirt3"),
+        Product("Devlopes Hustle", "$22", "shirt4") ,
         Product("Kickflip Studios", "$18", "shirt5")
-
     )
     val digitalGood = listOf<Product>()
     fun getProducts(category: String) :List<Product>{
